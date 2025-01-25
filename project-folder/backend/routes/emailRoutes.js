b25b1dd853bedc67021733a8edfced3d04e480d0
Riya Jain <riyajain0520@gmail.com>
2025-01-25 11:52:17 -0800

    updated env and app.py

--- project-folder/backend/routes/emailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchEmails } = require('../controllers/emailController');
-
-router.get('/emails', fetchEmails);
-
-module.exports = router;
